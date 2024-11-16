8c27a885525f7a8118c53a4c507581c078875198
thehazyobserver <114879476+thehazyobserver@users.noreply.github.com>
2024-11-16 14:18:41 -0500

    npm run build success

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,19 @@
 import { applyMiddleware, compose, createStore, combineReducers } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk"; // Named import
 import blockchainReducer from "./blockchain/blockchainReducer";
 import dataReducer from "./data/dataReducer";
 
+// Combine Reducers
 const rootReducer = combineReducers({
   blockchain: blockchainReducer,
   data: dataReducer,
 });
 
+// Middleware and Enhancers
 const middleware = [thunk];
 const composeEnhancers = compose(applyMiddleware(...middleware));
 
+// Configure Store
 const configureStore = () => {
   return createStore(rootReducer, composeEnhancers);
 };
